07bed27489d2392bf0284f5ebf5677df224d0647
Kyle Parris <124071575+kjp889@users.noreply.github.com>
2023-03-24 12:37:18 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,4 +5,26 @@
 #         Kyle Parris (1804904)
 #OS Programming Project
 
-print("Hello world")
+import datetime
+
+class Customer:
+  def __init__(cus, cusNum, arrivalTime, entryTime, shoppingTime, waitTime, exitTime):
+    cus.cusNum = cusNum
+    cus.arrivalTime = arrivalTime
+    cus.entryTime = entryTime
+    cus.shoppingTime = shoppingTime
+    cus.waitTime = waitTime
+    cus.exitTime = exitTime
+    
+  def __str__(cus):
+    return f"{cus.cusNum} - {cus.arrivalTime} - {cus.entryTime} - {cus.shoppingTime} - {cus.waitTime} - {cus.exitTime}"
+  
+#cus1 = Customer(1, 2, 3, 4)
+#print(cus1)
+
+inLine = []
+outLine = []
+doorIn = []
+doorOut = []
+
+
