a6daa4e237537aa9e8eb28ae07990d721eeeefba
Kyle Parris <124071575+kjp889@users.noreply.github.com>
2023-03-24 11:49:02 -0500

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,8 @@
+# Names:  Danielle Hemmings, 
+#         Tyandra Taylor, 
+#         Chadwick Hewitt, 
+#         Stephan Scott, 
+#         Kyle Parris (1804904)
+#OS Programming Project
+
+print("Hello world")
